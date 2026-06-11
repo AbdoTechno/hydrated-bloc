f18b3d46d817f496066a708a23ae648d8c9931e2
AbdoTechno <abdoalsenaweabdo@gmail.com>
2026-06-11 19:10:01 +0300

    bloc builder and provider

--- lib/controllers/cubit/counter_cubit.dart
+++ lib/controllers/cubit/counter_cubit.dart
@@ -4,5 +4,11 @@ part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterInitState());
-  
+  void incrementCounter() {
+    emit(CounterUpdateState(state.counterValue + 1));
+  }
+  void decrementCounter() {
+    emit(CounterUpdateState(state.counterValue - 1));
+  } 
+
 }
