d4770a14563a855e18babdad1b808666468c7f9d
AbdoTechno <abdoalsenaweabdo@gmail.com>
2026-06-12 00:55:52 +0300

    bloc data types

--- lib/controllers/cubit/counter_cubit.dart
+++ lib/controllers/cubit/counter_cubit.dart
@@ -12,3 +12,31 @@ class CounterCubit extends Cubit<CounterState> {
   } 
 
 }
+
+
+// if state is int we can directly emit int value  without creating state class 
+// class CounterCubit extends Cubit<int> {
+//   CounterCubit() : super(0);
+//   void incrementCounter() {
+//     emit(state + 1);
+//   }
+//   void decrementCounter() {
+//     emit(state - 1);
+//   }
+// }  
+// but it is not a good practice to emit primitive data type as state
+// because it will be difficult to manage the state in future 
+//when we have multiple states and also it will be difficult to debug the state changes.
+// so it is better to create state class and emit the state class object.
+
+// in ui we can directly use the state value without creating state class
+// but it is not a good practice to use primitive data type as state
+// because it will be difficult to manage the state in future
+// code example
+// int is the state of the cubit and we can directly use it in the ui without creating state class
+// BlocBuilder<CounterCubit, int>( 
+//   builder: (context, state) => Text(
+//     '$state',
+//     style: Theme.of(context).textTheme.headlineMedium,
+//   ),
+// ),
